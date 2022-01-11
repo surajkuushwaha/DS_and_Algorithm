146af7f21a5b4831fb8aa67a34b800b5692e8cea
Suraj kushwaha <skk74143@gmail.com>
2022-01-11 11:17:22 +0530

    LinkedList Implementation LL DLL CLL

---
+++ src/LinkedList/Main.java
@@ -0,0 +1,41 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+//        LL list = new LL();
+//        list.insertFirst(45);
+//        list.insertFirst(15);
+//        list.insertFirst(4);
+//        list.insertFirst(20);
+//        list.insertLast(99);
+//        list.insert(100, 3);
+//        list.display();
+//        list.deleteFirst();
+//        list.display();
+//        list.display();
+//        list.deleteLast();
+//        list.display();
+//        list.display();
+//        list.delete(2);
+//        list.display();
+//        System.out.println(list.find(4));
+//        Circular LL
+//        DLL list = new DLL();
+//        list.insertFirst(45);
+//        list.insertFirst(15);
+//        list.insertFirst(4);
+//        list.insertFirst(20);
+//        list.insertLast(99);
+//        list.insert(15,0);
+//        list.display();
+                CLL list = new CLL();
+        list.insert(15);
+        list.insert(45);
+        list.insert(4);
+        list.insert(20);
+        list.insert(99);
+        list.delete(45);
+        list.display();
+
+    }
+}
